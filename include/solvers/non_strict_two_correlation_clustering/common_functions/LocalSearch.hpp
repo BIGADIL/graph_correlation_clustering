67ffd3055fb605ba380b578d3d292b94374382ab
Alex <morshinin.alexander@gmail.com>
2021-01-30 02:08:32 +0600

    Fix localSearch from O(n^4) to O(n^3).

--- include/solvers/non_strict_two_correlation_clustering/common_functions/LocalSearch.hpp
+++ include/solvers/non_strict_two_correlation_clustering/common_functions/LocalSearch.hpp
@@ -1,40 +1,54 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 
 #include "../../../clustering/factories/IClusteringFactory.hpp"
 
 namespace non_strict_2cc {
 
-  /**
-   * Local search for NS2CC.
-   * @see Coleman, Saunderson and Wirth. A Local-Search 2-Approximation for 2-Correlation-Clustering.
-   */
-  class LocalSearch {
+/**
+ * Local search for NS2CC.
+ * @see Coleman, Saunderson and Wirth. A Local-Search 2-Approximation for 2-Correlation-Clustering.
+ */
+class LocalSearch {
 
-   private:
-    /**
-     * Compute local improvement of tossing vertex from one cluster to another.
-     *
-     * @param graph source graph.
-     * @param cur_clustering current clustering.
-     * @param vertex vertex to toss.
-     * @return local improvement of tossing vertex from one cluster to another.
-     */
-    static int ComputeLocalImprovement(const IGraph &graph,
-                                       const IClustPtr &cur_clustering,
-                                       const unsigned vertex);
+ private:
+  struct LocalSearchCandidate {
+    unsigned vertex;
+    int local_improvement;
 
    public:
-    /**
-     * Compute local optimal solution for the source graph.
-     *
-     * @param graph source graph.
-     * @param cur_clustering init clustering.
-     * @return local optimal clustering.
-     */
-    static IClustPtr ComputeLocalOptimum(const IGraph &graph,
-                                         const IClustPtr &cur_clustering);
+    LocalSearchCandidate(unsigned vertex, int local_improvement)
+        : vertex(vertex), local_improvement(local_improvement) {
+
+    }
+  };
+
+  static std::vector<int> InitLocalImprovements(const IGraph &graph,
+                                                const IClustPtr &cur_clustering);
+
+  static LocalSearchCandidate FindCandidate(const IGraph &graph,
+                                            const std::vector<int> &local_improvement_list);
+
+  static std::vector<int> UpdateLocalImprovements(const IGraph &graph,
+                                                  const IClustPtr &cur_clustering,
+                                                  std::vector<int> &local_improvement_list,
+                                                  const unsigned vertex);
+
+  static IClustPtr UpdateClustering(IClustPtr &cur_clustering,
+                                    const unsigned vertex);
+
+ public:
+  /**
+   * Compute local optimal solution for the source graph.
+   *
+   * @param graph source graph.
+   * @param cur_clustering init clustering.
+   * @return local optimal clustering.
+   */
+  static IClustPtr ComputeLocalOptimum(const IGraph &graph,
+                                       const IClustPtr &cur_clustering);
 };
 }
 
--- src/solvers/non_strict_two_correlation_clustering/common_functions/LocalSearch.cpp
+++ src/solvers/non_strict_two_correlation_clustering/common_functions/LocalSearch.cpp
@@ -1,48 +1,83 @@
 #include <climits>
 #include <iostream>
+#include <vector>
 
 #include "../../../../include/solvers/non_strict_two_correlation_clustering/common_functions/LocalSearch.hpp"
 
 IClustPtr non_strict_2cc::LocalSearch::ComputeLocalOptimum(const IGraph &graph,
                                                            const IClustPtr &cur_clustering) {
   auto result = cur_clustering->GetCopy();
+  auto local_improvement_list = InitLocalImprovements(graph, cur_clustering);
   while (true) {
-    int local_improvement = INT_MIN;
-    unsigned candidate = UINT_MAX;
-    for (unsigned i = 0; i < graph.Size(); i++) {
-      auto tmp_local_improvement = ComputeLocalImprovement(graph, result, i);
-      if (tmp_local_improvement > local_improvement) {
-        local_improvement = tmp_local_improvement;
-        candidate = i;
-      }
-    }
-    if (local_improvement <= 0) {
+    auto candidate = FindCandidate(graph, local_improvement_list);
+    if (candidate.local_improvement <= 0) {
       break;
     }
-    if (result->GetLabel(candidate) == FIRST_CLUSTER) {
-      result->SetupLabelForVertex(candidate, SECOND_CLUSTER);
-    } else {
-      result->SetupLabelForVertex(candidate, FIRST_CLUSTER);
-    }
+    local_improvement_list = UpdateLocalImprovements(graph, result, local_improvement_list, candidate.vertex);
+    result = UpdateClustering(result, candidate.vertex);
   }
   return result;
 }
 
-int non_strict_2cc::LocalSearch::ComputeLocalImprovement(const IGraph &graph,
-                                                         const IClustPtr &cur_clustering,
-                                                         const unsigned vertex) {
-  int local_improvement = 0;
+std::vector<int> non_strict_2cc::LocalSearch::InitLocalImprovements(const IGraph &graph,
+                                                                    const IClustPtr &cur_clustering) {
+  std::vector<int> local_improvement_list(graph.Size());
   for (unsigned i = 0; i < graph.Size(); i++) {
+    for (unsigned j = i + 1; j < graph.Size(); ++j) {
+      bool is_same_clustered = cur_clustering->IsSameClustered(i, j);
+      bool is_joined = graph.IsJoined(i, j);
+      if (is_same_clustered) {
+        local_improvement_list[i] += is_joined ? -1 : 1;
+        local_improvement_list[j] += is_joined ? -1 : 1;
+      } else {
+        local_improvement_list[i] += is_joined ? 1 : -1;
+        local_improvement_list[j] += is_joined ? 1 : -1;
+      }
+    }
+  }
+  return local_improvement_list;
+}
+
+non_strict_2cc::LocalSearch::LocalSearchCandidate non_strict_2cc::LocalSearch::FindCandidate(const IGraph &graph,
+                                                                                             const std::vector<int> &local_improvement_list) {
+  int local_improvement = INT_MIN;
+  unsigned candidate = UINT_MAX;
+  for (unsigned i = 0; i < graph.Size(); ++i) {
+    if (local_improvement_list[i] > local_improvement) {
+      local_improvement = local_improvement_list[i];
+      candidate = i;
+    }
+  }
+  return LocalSearchCandidate(candidate, local_improvement);
+}
+
+std::vector<int> non_strict_2cc::LocalSearch::UpdateLocalImprovements(const IGraph &graph,
+                                                                      const IClustPtr &cur_clustering,
+                                                                      std::vector<int> &local_improvement_list,
+                                                                      const unsigned vertex) {
+  local_improvement_list[vertex] = 0;
+  for (unsigned i = 0; i < graph.Size(); ++i) {
     if (i == vertex) {
       continue;
     }
-    bool is_same_clustered = cur_clustering->IsSameClustered(i, vertex);
-    bool is_joined = graph.IsJoined(i, vertex);
-    if (is_same_clustered) {
-      local_improvement += is_joined ? -1 : 1;
+    if ((graph.IsJoined(i, vertex) && cur_clustering->IsSameClustered(i, vertex))
+        || (!graph.IsJoined(i, vertex) && !cur_clustering->IsSameClustered(i, vertex))) {
+      local_improvement_list[i] += 2;
+      local_improvement_list[vertex] += 1;
     } else {
-      local_improvement += is_joined ? 1 : -1;
+      local_improvement_list[i] -= 2;
+      local_improvement_list[vertex] -= 1;
     }
   }
-  return local_improvement;
+  return local_improvement_list;
+}
+
+IClustPtr non_strict_2cc::LocalSearch::UpdateClustering(IClustPtr &cur_clustering,
+                                                        const unsigned int vertex) {
+  if (cur_clustering->GetLabel(vertex) == FIRST_CLUSTER) {
+    cur_clustering->SetupLabelForVertex(vertex, SECOND_CLUSTER);
+  } else {
+    cur_clustering->SetupLabelForVertex(vertex, FIRST_CLUSTER);
+  }
+  return cur_clustering;
 }
