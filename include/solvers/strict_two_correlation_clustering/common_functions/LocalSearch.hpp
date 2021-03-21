0576146464bfcc66fe6e3a1c681a89788508062d
Alex <morshinin.alexander@gmail.com>
2021-03-21 17:54:38 +0600

    Edit local search for s2cc

--- include/solvers/strict_two_correlation_clustering/common_functions/LocalSearch.hpp
+++ include/solvers/strict_two_correlation_clustering/common_functions/LocalSearch.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 #include "../../../clustering/factories/IClusteringFactory.hpp"
 
 namespace strict_2cc {
@@ -10,19 +11,51 @@ namespace strict_2cc {
  * @see Coleman, Saunderson and Wirth. A Local-Search 2-Approximation for 2-Correlation-Clustering.
  */
 class LocalSearch {
+ private:
 
  private:
-  /**
-   * Compute local improvement of tossing vertex from one cluster to another.
-   *
-   * @param graph source graph.
-   * @param cur_clustering current clustering.
-   * @param vertex vertex to toss.
-   * @return local improvement of tossing vertex from one cluster to another.
-   */
-  static int ComputeLocalImprovement(const IGraph &graph,
-                                     const IClustPtr &cur_clustering,
-                                     unsigned vertex);
+  struct ExcludeVertices {
+    unsigned vertex;
+    unsigned opposite_vertex;
+
+    ExcludeVertices(unsigned vertex, unsigned opposite_vertex)
+        : vertex(vertex), opposite_vertex(opposite_vertex) {
+
+    }
+
+    bool contain(unsigned i) const {
+      return i == vertex || i == opposite_vertex;
+    }
+  };
+
+  struct LocalSearchCandidate {
+    unsigned vertex;
+    int local_improvement;
+
+   public:
+    LocalSearchCandidate(unsigned vertex, int local_improvement)
+        : vertex(vertex), local_improvement(local_improvement) {
+
+    }
+  };
+
+ private:
+  static std::vector<int> InitLocalImprovements(const IGraph &graph,
+                                                const IClustPtr &cur_clustering,
+                                                const ExcludeVertices exclude_vertices);
+
+  static LocalSearchCandidate FindCandidate(const IGraph &graph,
+                                            const std::vector<int> &local_improvement_list,
+                                            const ExcludeVertices exclude_vertices);
+
+  static std::vector<int> UpdateLocalImprovements(const IGraph &graph,
+                                                  const IClustPtr &cur_clustering,
+                                                  std::vector<int> &local_improvement_list,
+                                                  const unsigned vertex,
+                                                  const ExcludeVertices exclude_vertices);
+
+  static IClustPtr UpdateClustering(IClustPtr &cur_clustering,
+                                    const unsigned vertex);
 
  public:
   /**
--- src/solvers/strict_two_correlation_clustering/common_functions/LocalSearch.cpp
+++ src/solvers/strict_two_correlation_clustering/common_functions/LocalSearch.cpp
@@ -7,44 +7,86 @@ IClustPtr strict_2cc::LocalSearch::ComputeLocalOptimum(const IGraph &graph,
                                                        const unsigned vertex,
                                                        const unsigned opposite_vertex) {
   auto result = cur_clustering->GetCopy();
+  auto exclude_vertices = ExcludeVertices(vertex, opposite_vertex);
+  auto local_improvement_list = InitLocalImprovements(graph, cur_clustering, exclude_vertices);
   while (true) {
-    int local_improvement = INT_MIN;
-    unsigned candidate = UINT_MAX;
-    for (unsigned i = 0; i < graph.Size(); i++) {
-      if (i == vertex || i == opposite_vertex) continue;
-      auto tmp_local_improvement = ComputeLocalImprovement(graph, result, i);
-      if (tmp_local_improvement > local_improvement) {
-        local_improvement = tmp_local_improvement;
-        candidate = i;
-      }
-    }
-    if (local_improvement <= 0) {
+    auto candidate = FindCandidate(graph, local_improvement_list, exclude_vertices);
+    if (candidate.local_improvement <= 0) {
       break;
     }
-    if (result->GetLabel(candidate) == 0) {
-      result->SetupLabelForVertex(candidate, SECOND_CLUSTER);
-    } else {
-      result->SetupLabelForVertex(candidate, FIRST_CLUSTER);
-    }
+    local_improvement_list = UpdateLocalImprovements(graph, result, local_improvement_list, candidate.vertex, exclude_vertices);
+    result = UpdateClustering(result, candidate.vertex);
   }
   return result;
 }
 
-int strict_2cc::LocalSearch::ComputeLocalImprovement(const IGraph &graph,
-                                                     const IClustPtr &cur_clustering,
-                                                     const unsigned vertex) {
-  int local_improvement = 0;
+std::vector<int> strict_2cc::LocalSearch::InitLocalImprovements(const IGraph &graph,
+                                                                const IClustPtr &cur_clustering,
+                                                                const strict_2cc::LocalSearch::ExcludeVertices exclude_vertices) {
+  std::vector<int> local_improvement_list(graph.Size());
   for (unsigned i = 0; i < graph.Size(); i++) {
-    if (i == vertex) {
+    if (exclude_vertices.contain(i)) {
       continue;
     }
-    bool is_same_clustered = cur_clustering->IsSameClustered(i, vertex);
-    bool is_joined = graph.IsJoined(i, vertex);
-    if (is_same_clustered) {
-      local_improvement += is_joined ? -1 : 1;
-    } else {
-      local_improvement += is_joined ? 1 : -1;
+    for (unsigned j = i + 1; j < graph.Size(); ++j) {
+      bool is_same_clustered = cur_clustering->IsSameClustered(i, j);
+      bool is_joined = graph.IsJoined(i, j);
+      if (is_same_clustered) {
+        local_improvement_list[i] += is_joined ? -1 : 1;
+        local_improvement_list[j] += is_joined ? -1 : 1;
+      } else {
+        local_improvement_list[i] += is_joined ? 1 : -1;
+        local_improvement_list[j] += is_joined ? 1 : -1;
+      }
     }
   }
-  return local_improvement;
+  return local_improvement_list;
+}
+
+strict_2cc::LocalSearch::LocalSearchCandidate strict_2cc::LocalSearch::FindCandidate(const IGraph &graph,
+                                                                                     const std::vector<int> &local_improvement_list,
+                                                                                     const strict_2cc::LocalSearch::ExcludeVertices exclude_vertices) {
+  int local_improvement = INT_MIN;
+  unsigned candidate = UINT_MAX;
+  for (unsigned i = 0; i < graph.Size(); ++i) {
+    if (exclude_vertices.contain(i)) {
+      continue;
+    }
+    if (local_improvement_list[i] > local_improvement) {
+      local_improvement = local_improvement_list[i];
+      candidate = i;
+    }
+  }
+  return LocalSearchCandidate(candidate, local_improvement);
+}
+
+std::vector<int> strict_2cc::LocalSearch::UpdateLocalImprovements(const IGraph &graph,
+                                                                  const IClustPtr &cur_clustering,
+                                                                  std::vector<int> &local_improvement_list,
+                                                                  const unsigned int vertex,
+                                                                  const strict_2cc::LocalSearch::ExcludeVertices exclude_vertices) {
+  local_improvement_list[vertex] = 0;
+  for (unsigned i = 0; i < graph.Size(); ++i) {
+    if (i == vertex || exclude_vertices.contain(i)) {
+      continue;
+    }
+    if ((graph.IsJoined(i, vertex) && cur_clustering->IsSameClustered(i, vertex))
+        || (!graph.IsJoined(i, vertex) && !cur_clustering->IsSameClustered(i, vertex))) {
+      local_improvement_list[i] += 2;
+      local_improvement_list[vertex] += 1;
+    } else {
+      local_improvement_list[i] -= 2;
+      local_improvement_list[vertex] -= 1;
+    }
+  }
+  return local_improvement_list;
+}
+
+IClustPtr strict_2cc::LocalSearch::UpdateClustering(IClustPtr &cur_clustering, const unsigned int vertex) {
+  if (cur_clustering->GetLabel(vertex) == FIRST_CLUSTER) {
+    cur_clustering->SetupLabelForVertex(vertex, SECOND_CLUSTER);
+  } else {
+    cur_clustering->SetupLabelForVertex(vertex, FIRST_CLUSTER);
+  }
+  return cur_clustering;
 }
